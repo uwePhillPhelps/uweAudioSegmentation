d67d81e3ceb874fff90c840cf4e22065d2d378f0
Phill Phelps <philip3.phelps@uwe.ac.uk>
2011-08-03 13:13:33 +0100

    phpTools pkg installer DOES NOT WORK properly - script fails sporadically

--- installerScripts/enablePhp5Apache2.sh
+++ installerScripts/enablePhp5Apache2.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-# modify /etc/apache2/httpd.conf to enable loading of php5_module
-sed -i 's/^#LoadModule php5_module/LoadModule php5_module/' /etc/apache2/httpd.conf"
+# when calling sed in a script, no need for '' quotes around command
+
+sudo sed -ie s/^#LoadModule php5_module/LoadModule php5_module/ /etc/apache2/httpd.conf
+
