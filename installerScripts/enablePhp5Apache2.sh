0c715a9ea1f12fae4be77632e5b8347346b061a1
Phill Phelps <philip3.phelps@uwe.ac.uk>
2011-08-10 09:18:59 +0100

    modified enablePhp5Apache2.sh to escape whitespace

--- installerScripts/enablePhp5Apache2.sh
+++ installerScripts/enablePhp5Apache2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # when calling sed in a script, no need for '' quotes around command
 
-sudo sed -ie s/^#LoadModule php5_module/LoadModule php5_module/ /etc/apache2/httpd.conf
+sudo sed -ie s/^#LoadModule\ php5_module/LoadModule\ php5_module/ /etc/apache2/httpd.conf
 
